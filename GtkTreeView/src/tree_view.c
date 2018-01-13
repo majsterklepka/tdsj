c790589e6fabc6b45c03637518d83e28d8081856
Majster Klepka <48721262935pl@gmail.com>
2018-01-13 03:52:05 +0100

    I do a some modification of original file, now TreeView is with header and have and has a larger width and is homogeneous and of course speak Polish!

--- GtkTreeView/src/tree_view.c
+++ GtkTreeView/src/tree_view.c
@@ -20,7 +20,7 @@ init_list(GtkWidget *list){
 	GtkListStore *store;
 
 	renderer = gtk_cell_renderer_text_new();
-	column = gtk_tree_view_column_new_with_attributes("List items", renderer, "text", LIST_ITEM, NULL);
+	column = gtk_tree_view_column_new_with_attributes("Elementy listy", renderer, "text", LIST_ITEM, NULL);
 	gtk_tree_view_append_column(GTK_TREE_VIEW(list), column);
 
 	store = gtk_list_store_new(N_COLUMNS, G_TYPE_STRING);
@@ -71,11 +71,13 @@ main(int argc, char **argv){
 	gtk_window_set_title(GTK_WINDOW(window), "widok listy");
 	gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
 	gtk_container_set_border_width(GTK_CONTAINER(window), 10);
-	gtk_window_set_default_size(GTK_WINDOW(window), 300, 250);
-	gtk_tree_view_set_headers_visible(GTK_TREE_VIEW(list), FALSE);
+	gtk_window_set_default_size(GTK_WINDOW(window), 400, 250);
+	gtk_tree_view_set_headers_visible(GTK_TREE_VIEW(list), TRUE);
 	label = gtk_label_new("");
-	vbox = gtk_box_new(FALSE, 0);
+	vbox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 5);
 	gtk_box_pack_start(GTK_BOX(vbox), list, TRUE, TRUE, 5);
+	gtk_box_pack_start(GTK_BOX(vbox), label, TRUE, TRUE, 5);
+	gtk_box_set_homogeneous(GTK_BOX(vbox), TRUE);
 	gtk_container_add(GTK_CONTAINER(window), vbox);
 
 	init_list(list);
