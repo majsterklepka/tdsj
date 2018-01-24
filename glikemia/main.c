a0540320b510683fb33e3e45f77d77fa1f5363c4
Majster Klepka <48721262935pl@gmail.com>
2018-01-24 10:20:21 +0100

    new sample file with calculation of the average of three months of glycemia fully written in C with libc and text based interface

---
+++ glikemia/main.c
@@ -0,0 +1,31 @@
+/*
+ * main.c
+ *
+ *  Created on: 24 sty 2018
+ *      Author: majsterklepka
+ *     License: GPL-3.0
+ */
+
+#include <stdlib.h>
+#include <locale.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+	char * locale_info = setlocale(LC_ALL, "");
+	char *locale = "pl_PL.UTF-8";
+	char *saved_locale = strdup(locale_info);
+	if (strcmp(saved_locale, locale) != 0){
+		printf("I setup application language to : %s\n", locale);
+		locale_info = setlocale(LC_ALL, locale);
+		if (locale_info == NULL){
+			printf("You don't have Polish locale installed!!!\nPlease, install it!\nExit!\n");
+			return EXIT_FAILURE;
+		}
+	}
+	printf ("ustawiono język aplikacji i kodowanie znaków\nna: \t\t\t\t%s\n", locale_info);
+
+	return EXIT_SUCCESS;
+}
+
